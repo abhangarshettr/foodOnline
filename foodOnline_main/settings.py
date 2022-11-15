f4a14bcc0edbdc55e5f2e2ad956cd9320b57708e
Aishwarya Bhangarshettra <bhangarshettra.aishwarya@gmail.com>
2022-11-14 20:15:44 -0800

    setting up postgres, pgadmin and creation of required DB

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'foodOnline_main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME' : 'foodOnline_db',
+        'USER' : 'postgres',
+        'PASSWORD' : 'Compe561',
+        'HOST' : 'localhost',
+        'PORT': '5432',
     }
 }
 
