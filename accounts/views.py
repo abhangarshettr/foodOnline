889337f8f021d0663187bf0914f8c15acf9e2d6e
Aishwarya Bhangarshettra <bhangarshettra.aishwarya@gmail.com>
2022-11-16 15:18:01 -0800

    Registration form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserForm
 
 # Create your views here.
 
 def registerUser(request):
-    return HttpResponse(' This is a user')
+    form = UserForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/registerUser.html',context)
