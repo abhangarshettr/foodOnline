8dbc0ceff5351488f57cb7d88db2c16d5783c680
Aishwarya Bhangarshettra <bhangarshettra.aishwarya@gmail.com>
2022-11-16 00:24:44 -0800

    changes compeleted

--- accounts/models.py
+++ accounts/models.py
@@ -100,24 +100,24 @@ class UserProfile(models.Model):
     def __str__(self):
         return self.user.email
     
-@receiver(post_save,sender=User)
-def post_save_create_profile_receiver(sender,instance,created,**kwargs):
-    print(created)
-    if created:
-        UserProfile.objects.create(user=instance)
-        print("User profile created ")
-    else:
-        try: 
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-        except:
-            # create the user profile if not exists
-            UserProfile.objects.create(user=instance)    
-            print("Profile was not present, created one")    
-        print("User is updated")
+# @receiver(post_save,sender=User)
+# def post_save_create_profile_receiver(sender,instance,created,**kwargs):
+#     print(created)
+#     if created:
+#         UserProfile.objects.create(user=instance)
+#         print("User profile created ")
+#     else:
+#         try: 
+#             profile = UserProfile.objects.get(user=instance)
+#             profile.save()
+#         except:
+#             # create the user profile if not exists
+#             UserProfile.objects.create(user=instance)    
+#             print("Profile was not present, created one")    
+#         print("User is updated")
         
-@receiver(pre_save,sender=User)
-def pre_save_profile_receiver(sender,instance,**kwargs):
-    print(instance.username,'This user is being saved')
+# @receiver(pre_save,sender=User)
+# def pre_save_profile_receiver(sender,instance,**kwargs):
+#     print(instance.username,'This user is being saved')
     
         
