0c640540932b665eb76cebe38cad5afeef25cc0e
Aishwarya Bhangarshettra <bhangarshettra.aishwarya@gmail.com>
2022-11-16 22:14:59 -0800

    Adding messages

--- accounts/forms.py
+++ accounts/forms.py
@@ -8,4 +8,13 @@ class UserForm(forms.ModelForm):
         model = User
         fields = ['first_name','last_name','username','email','password']
         
+    def clean(self):
+        cleaned_data = super(UserForm,self).clean()
+        password = cleaned_data.get('password')
+        confirm_password=cleaned_data.get('confirm_password')
+        
+        if password != confirm_password:
+            raise forms.ValidationError("Password doesn't match ")
+        
+        
         
--- accounts/views.py
+++ accounts/views.py
@@ -3,18 +3,37 @@ from django.http import HttpResponse
 from .forms import UserForm
 from django.shortcuts import redirect
 from .models import User
+from django.contrib import messages
 
 # Create your views here.
 
 def registerUser(request):
     if request.method == 'POST':
-        print(request.POST)
+        #print()
         form = UserForm(request.POST)
+        
         if form.is_valid():
-            user = form.save(commit=False)
-            user.role = User.CUSTOMER
-            form.save()
+            # create the user using the form
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.role = User.CUSTOMER
+            # user.set_password(password)
+            # form.save()
+            
+            #create the user using create_user method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email=email,password=password)
+            user.ROLE = User.CUSTOMER
+            user.save()
+            messages.success(request,'Your account has been registered succesfully')
             return redirect('registerUser')
+        else:
+            print('Invalid Form')
+            print(form.errors)
             
     else:
         form = UserForm()
